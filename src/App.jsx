6d814bbf72b86924771d1d0849029d4edd293ff9
fc <fc@gsmk.de>
2023-02-24 12:11:38 +0100

    add tailwind to solid project

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import styles from './App.module.css';
 function App() {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
+      <header  class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
+        <h1 class="text-3xl font-bold underline">TailwindCSS</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
